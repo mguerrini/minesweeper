7c51a595938d358b4d661375fb975ab9220f0b70
Maximiliano Guerrini <maximiliano.guerrini@mercadolibre.com>
2020-12-23 14:30:45 -0300

    Get port from environment

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/minesweeper/src/common/logger"
 	"github.com/minesweeper/src/server"
+	"os"
 )
 
 func main(){
@@ -13,5 +14,10 @@ func main(){
 	engine := server.New()
 	logger.Info("Listening and serving HTTP on port 8080")
 
-	engine.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	engine.Run(port)
 }
